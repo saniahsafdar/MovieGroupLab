3d0d1b23620038f5865a78387222db4e50fc42a8
Nick Maxwell <NMaxwell93@gmail.com>
2021-05-20 15:03:48 -0500

    radio genres

--- src/Component/MovieForm.tsx
+++ src/Component/MovieForm.tsx
@@ -27,14 +27,20 @@ function MovieForm() {
           Year
           <input type="number" value={year} onChange={e => setYear(Number(e.target.value))} />
         </label>
-        <label>
-          Genre
-          <input type="number" value={genre} onChange={e => setGenre(Number(e.target.value))}/>
-        </label>
+
         <label>
           Maximum Runtime
           <input type="number" value={maxRuntime} onChange={e => setMaxRuntime(Number(e.target.value))}/>
-        </label>
+        </label>        
+
+        <div>
+          <p>Select a genre!</p>
+        <input type="radio" value="18" name="genre" onChange={e => setGenre(Number(e.target.value))} /> Drama
+        <input type="radio" value="28" name="genre" onChange={e => setGenre(Number(e.target.value))} /> Action
+        <input type="radio" value="35" name="genre" onChange={e => setGenre(Number(e.target.value))}/> Comedy
+        <input type="radio" value="16" name="genre" onChange={e => setGenre(Number(e.target.value))}/> Animation
+        <input type="radio" value="27" name="genre" onChange={e => setGenre(Number(e.target.value))}/> Horror
+      </div>
         <button type="submit">Search</button>
       </div>
     </form>
