7f74fd6f5328533ac237040542f6f67037ba9d43
luykl <leah.logsin@gmail.com>
2021-05-22 14:56:07 -0400

    form values

--- src/Component/MovieForm.tsx
+++ src/Component/MovieForm.tsx
@@ -2,12 +2,12 @@ import { FormEvent, useState } from "react";
 import MovieResults from "./MovieResults";
 
 function MovieForm() {
-  const [year, setYear] = useState("")
-  const [submittedYear, setSubmittedYear] = useState("")
-  const [genre, setGenre] = useState("")
-  const [submittedGenre, setSubmittedGenre] = useState("")
-  const [maxRuntime, setMaxRuntime] = useState("")
-  const [submittedMaxRuntime, setSubmittedMaxRuntime] = useState("")
+  const [year, setYear] = useState(2021)
+  const [submittedYear, setSubmittedYear] = useState(0)
+  const [genre, setGenre] = useState(18)
+  const [submittedGenre, setSubmittedGenre] = useState(0)
+  const [maxRuntime, setMaxRuntime] = useState(0)
+  const [submittedMaxRuntime, setSubmittedMaxRuntime] = useState(0)
 
   
   function handleSubmit(e:FormEvent) {
@@ -26,22 +26,22 @@ function MovieForm() {
         
         <label>Select a genre: </label> <br />    
         <div className="radios">
-        <label><input type="radio" value="18" name="genre" onChange={e => setGenre(e.target.value)} /> Drama</label>
-        <label><input type="radio" value="28" name="genre" onChange={e => setGenre(e.target.value)} /> Action</label>
-        <label><input type="radio" value="35" name="genre" onChange={e => setGenre(e.target.value)}/> Comedy</label>
-        <label><input type="radio" value="16" name="genre" onChange={e => setGenre(e.target.value)}/> Animation</label>
-        <label><input type="radio" value="27" name="genre" onChange={e => setGenre(e.target.value)}/> Horror</label>
+        <label><input type="radio" value="18" name="genre" onChange={e => setGenre(Number(e.target.value))} /> Drama</label>
+        <label><input type="radio" value="28" name="genre" onChange={e => setGenre(Number(e.target.value))} /> Action</label>
+        <label><input type="radio" value="35" name="genre" onChange={e => setGenre(Number(e.target.value))}/> Comedy</label>
+        <label><input type="radio" value="16" name="genre" onChange={e => setGenre(Number(e.target.value))}/> Animation</label>
+        <label><input type="radio" value="27" name="genre" onChange={e => setGenre(Number(e.target.value))}/> Horror</label>
       </div>
       <p>
         <label>
           Year: <br />
-          <input type="number" min="1900" value={year} onChange={e => setYear(e.target.value)} />
+          <input type="number" min="1900" value={year} onChange={e => setYear(Number(e.target.value))} />
         </label>
         </p>
         <p>
         <label>
           Max Runtime: <br />
-          <input type="number" value={maxRuntime} min="0" onChange={e => setMaxRuntime(e.target.value)}/>
+          <input type="number" value={maxRuntime} min="0" onChange={e => setMaxRuntime(Number(e.target.value))}/>
         </label>    
         </p>
         <button type="submit">Find Movies</button>
--- src/Services/MovieAPIService.tsx
+++ src/Services/MovieAPIService.tsx
@@ -2,7 +2,7 @@ import {Movie} from "../Model/Movie"
 
 const movieAPIKey: string = process.env.REACT_APP_MOVIE_API_CREDENTIALS || "";
 
-export function fetchByAll(year: string, genre: string, maxRuntime: string): Promise<Movie[]> {
+export function fetchByAll(year: number, genre: number, maxRuntime: number): Promise<Movie[]> {
     return fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&with_genres=${genre}&primary_release_year=${year}&with_runtime.lte=${maxRuntime}&api_key=${movieAPIKey}`)
 
     .then(res => res.json()) 
