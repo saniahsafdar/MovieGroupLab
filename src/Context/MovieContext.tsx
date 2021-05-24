1a864b7bdad2cba5267fbadb9c7dc8d294b84b3a
Nick Maxwell <NMaxwell93@gmail.com>
2021-05-24 09:40:41 -0500

    only adding once, buttons disabling

--- src/Context/MovieContext.tsx
+++ src/Context/MovieContext.tsx
@@ -1,12 +1,12 @@
-import { createContext, ReactNode, useState } from "react"
-import {Movie} from "../Model/Movie";
+import { createContext, ReactNode, useState } from "react";
+import { Movie } from "../Model/Movie";
 
 // Lists what is going to be in our context
 interface MovieContextValue {
   favoriteMovies: Movie[];
   addMovie: (movie: Movie) => void;
   removeMovie: (movie: Movie) => void;
-  isOnList: (movie: Movie) => boolean
+  isOnList: (movie: Movie) => boolean;
 }
 
 // necessary to provide blank/default values
@@ -14,37 +14,42 @@ const defaultValue: MovieContextValue = {
   favoriteMovies: [],
   addMovie: () => {},
   removeMovie: () => {},
-  isOnList: () => {return false}
-}
+  isOnList: () => {
+    return false;
+  },
+};
 
 // Here is the actual context object.
 export const MovieContext = createContext(defaultValue);
 
-
 export function MovieContextProvider({ children }: { children: ReactNode }) {
-  const [ favoriteMovies, setFavoriteMovies ] = useState<Movie[]>([
-  ]);
+  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
 
   function addMovie(movie: Movie): void {
-    setFavoriteMovies(prev => [ ...prev, { ...movie}]);
+    if (isOnList(movie)) {
+      console.log(`${movie.title} is already on your list ${movie.id} `);
+    } else {
+      setFavoriteMovies((prev) => [...prev, { ...movie }]);
+    }
   }
 
   function removeMovie(movie: Movie): void {
-    const index = favoriteMovies.indexOf(movie)
-    setFavoriteMovies(prevMovie => [
-          ...prevMovie.slice(0,index),
-          ...prevMovie.slice(index+1)
-      ])
+    const index = favoriteMovies.indexOf(movie);
+    setFavoriteMovies((prevMovie) => [
+      ...prevMovie.slice(0, index),
+      ...prevMovie.slice(index + 1),
+    ]);
   }
 
-  function isOnList(movie: Movie):boolean {
-    return favoriteMovies.some(movie => movie.id === movie.id)
+  function isOnList(movie: Movie) {
+    return favoriteMovies.some((existingMovie) => existingMovie.id === movie.id);
   }
 
   return (
-    <MovieContext.Provider value={{favoriteMovies, addMovie, removeMovie, isOnList}} >
+    <MovieContext.Provider
+      value={{ favoriteMovies, addMovie, removeMovie, isOnList }}
+    >
       {children}
     </MovieContext.Provider>
   );
 }
-
